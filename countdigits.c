72ea650e839f3b932facfd4cc6d3cba38bec0747
Surendra-ji <Thesurendra2.0@gmail.com>
2020-07-18 15:27:00 +0530

    To Count Digits in String

---
+++ countdigits.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+int a[10]={0,0,0,0,0,0,0,0,0},n;   
+char *s;
+   
+    s = malloc(1 * sizeof(char));
+    scanf("%[^\n]", s);
+    int j=strlen(s);
+
+    while(j)
+    {
+        if((int)*s>=48&&(int)*s<=57)
+        switch((int)*s)
+        {
+            case 48:
+            a[0]++;
+             break;
+            case 49:
+            a[1]++;
+             break;
+            case 50:
+            a[2]++;
+            break;
+            case 51:
+            a[3]++;
+             break;
+            case 52:
+            a[4]++;
+             break;
+            case 53:
+            a[5]++;
+             break;
+            case 54:
+            a[6]++;
+             break;
+            case 55:
+            a[7]++;
+             break;
+            case 56:
+            a[8]++;
+             break;
+            case 57:
+            a[9]++;
+             break;
+             default:
+             n++;
+        }
+        s++;
+     j--;
+    }
+    for(int i=0;i<10;i++)
+    printf("%d ",a[i]);
+
+    return 0;
+}
